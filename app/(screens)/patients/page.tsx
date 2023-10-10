cf0d28d66dd2861ce3a1b696e55b92a489de4253
lebelokamogelo <lebelokamogelo47@gmail.com>
2023-10-10 14:29:51 +0200

    Instand create account

--- app/(screens)/patients/page.tsx
+++ app/(screens)/patients/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Table,
   TableBody,
@@ -9,7 +11,13 @@ import {
 } from "@/components/ui/table"
 import { Settings2, X } from "lucide-react"
 
-import Account from "@/components/account/Account"
+import { auth, db } from "@/model/firebase"
+import { createUserWithEmailAndPassword } from "@firebase/auth"
+import { doc, setDoc } from "@firebase/firestore"
+import { Eye, EyeOff } from "lucide-react"
+import { toast } from "react-hot-toast"
+import { PulseLoader } from "react-spinners"
+
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -23,27 +31,76 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Dialog, DialogTrigger } from "@/components/ui/dialog"
 import { nanoid } from "nanoid"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 import DeleteUser from "./DeleteUser"
 
-type Props = {
+type PatientProps = {
   uuid: string
   name: string
   email: string
   phone: string
 }
 
-export default async function Patients() {
-  let data = []
+export default function Patients() {
+  const [data, setData] = useState<Array<PatientProps>>([])
 
-  const res = await fetch("http://localhost:3000/api/patients", {
-    method: "GET",
-    headers: { "Content-type": "application/json;charset=UTF-8" },
-    cache: "no-cache",
-  })
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [password, setPassword] = useState("")
+  const [show, setShow] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-  if (res.ok) {
-    data = await res.json()
+  const createAccount = (
+    email: string,
+    password: string,
+    phone: string,
+    name: string
+  ) => {
+    setLoading(true)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user
+
+        const docRef = await setDoc(doc(db, "patients", user.uid), {
+          name: name,
+          image: "",
+          email: email,
+          phone: phone,
+          uuid: user.uid,
+        })
+          .then(() => {
+            setData((prev) => [...prev, { uuid: user.uid, name, email, phone }])
+            toast.success("Account created successfully")
+            setLoading(false)
+          })
+          .catch((err) => {
+            toast.success(err.message)
+            setLoading(false)
+          })
+      })
+      .catch((error) => {
+        const errorMessage = error.message
+        toast.error(errorMessage)
+        setLoading(false)
+      })
   }
+
+  useEffect(() => {
+    const getPatients = async () => {
+      const res = await fetch("http://localhost:3000/api/patients", {
+        method: "GET",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
+        cache: "no-cache",
+      })
+
+      if (res.ok) {
+        setData(await res.json())
+      }
+    }
+
+    getPatients()
+  }, [])
 
   const avatarFallBackName = (name: string) =>
     name
@@ -68,7 +125,72 @@ export default async function Patients() {
             </AlertDialogCancel>
             <AlertDialogHeader>
               <AlertDialogTitle>
-                <Account />
+                <div className="bg-grey-lighter flex flex-col">
+                  <div className="container max-w-xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div className="bg-white px-6 py-8 rounded text-black w-full">
+                      <h1 className="mb-8 text-xl text-center">
+                        Create Account
+                      </h1>
+                      <input
+                        type="text"
+                        className="input-border text-base text-slate-800"
+                        name="fullname"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Full Name"
+                      />
+
+                      <input
+                        type="text"
+                        className="input-border text-base text-slate-800"
+                        name="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                      />
+
+                      <input
+                        type="text"
+                        className="input-border text-base text-slate-800"
+                        name="phone"
+                        placeholder="Phone Number"
+                        onChange={(e) => setPhone(e.target.value)}
+                        value={phone}
+                      />
+
+                      <div className="flex items-center border mb-4 border-gray-300 rounded-md p-0.5 input-password">
+                        <input
+                          id="Password"
+                          type={show ? "text" : "password"}
+                          className="flex-1 w-full h-full px-4 py-2 rounded-md focus:outline-none text-base text-slate-800"
+                          placeholder="Password"
+                          onChange={(e) => setPassword(e.target.value)}
+                          value={password}
+                        />
+                        <div
+                          className="p-1 mx-2 rounded-md show-hide-password hover:bg-slate-200"
+                          onClick={() => setShow((prev) => !prev)}
+                        >
+                          {show ? <Eye /> : <EyeOff />}
+                        </div>
+                      </div>
+                      <div className="loading mt-20 flex justify-center">
+                        {loading ? (
+                          <PulseLoader color="#36d7b7" />
+                        ) : (
+                          <button
+                            className="border-0 text-white text-base bg-slate-600 hover:bg-slate-700 w-full p-2.5 rounded-md outline-none"
+                            onClick={() => {
+                              createAccount(email, password, phone, name)
+                            }}
+                          >
+                            Create Accout
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </AlertDialogTitle>
             </AlertDialogHeader>
             <AlertDialogFooter></AlertDialogFooter>
@@ -93,7 +215,7 @@ export default async function Patients() {
         </TableHeader>
         <TableBody>
           {data &&
-            data.map((patient: Props) => (
+            data.map((patient: PatientProps) => (
               <TableRow key={nanoid()}>
                 <TableCell>
                   <Avatar className="h-8 w-8">
